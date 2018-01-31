616aa6e09479a6e5f100d56b49d2d738b0bc3470
Mojken <mikaelgillis@gmail.com>
2018-01-31 19:36:05 +0100

    Fixed "buildpath"

---
+++ src/main/java/com/deepztream/slaetester/SLAETester.java
@@ -0,0 +1,11 @@
+package com.deepztream.slaetester;
+
+import com.deepztream.slae.Version;
+
+public class SLAETester {
+
+    public static void main(String[] args) {
+        System.out.println("SLAE Version " + Version.getVersion());
+    }
+
+}
