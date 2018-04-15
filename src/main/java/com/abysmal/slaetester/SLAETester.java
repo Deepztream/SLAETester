ac9c6a61c1c4ee85e6be6c45b369492ea74028f9
Mojken <mikaelgillis@gmail.com>
2018-04-15 16:01:54 +0200

    No longer has direct access to the message bus

--- src/main/java/com/abysmal/slaetester/SLAETester.java
+++ src/main/java/com/abysmal/slaetester/SLAETester.java
@@ -2,7 +2,6 @@ package com.abysmal.slaetester;
 
 import com.abysmal.slae.SLAE;
 import com.abysmal.slae.message.Message;
-import com.abysmal.slae.message.MessageBus;
 import com.abysmal.slae.object.GUIObject;
 import com.abysmal.slae.system.Render;
 import com.abysmal.slae.system.System;
@@ -20,7 +19,7 @@ public class SLAETester implements System {
 		String shaderPath = SLAETester.class.getResource("/shaders/Basic.shader").getPath();
 		Object[] data = { Render.MAIN_MENU,
 				new GUIObject(new Rectanglef(-0.5f, -0.5f, 0.5f, 0.5f), new Vector3f(0.6f, 0.3f, 0.0f), shaderPath) };
-		MessageBus.getBus().postMessage(new Message("Add GUIObject", data));
+		SLAE.execute("Add GUIObject", data);
 	}
 
 	@Override
