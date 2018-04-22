5870b53d442d2fb3c662b66c10a53b834aa5989d
Mojken <mikaelgillis@gmail.com>
2018-04-22 18:37:04 +0200

    Add button in top corner

--- src/main/java/com/abysmal/slaetester/SLAETester.java
+++ src/main/java/com/abysmal/slaetester/SLAETester.java
@@ -1,13 +1,16 @@
 package com.abysmal.slaetester;
 
+import java.awt.Color;
+
 import com.abysmal.slae.SLAE;
 import com.abysmal.slae.message.Message;
 import com.abysmal.slae.object.GUIObject;
+import com.abysmal.slae.object.HUDObject;
 import com.abysmal.slae.system.Render;
 import com.abysmal.slae.system.System;
 
 import org.joml.Rectanglef;
-import org.joml.Vector3f;
+import org.joml.Vector2i;
 
 public class SLAETester implements System {
 
@@ -17,9 +20,14 @@ public class SLAETester implements System {
 
 	public void init() {
 		String shaderPath = SLAETester.class.getResource("/shaders/Basic.shader").getPath();
-		Object[] data = { Render.MAIN_MENU,
-				new GUIObject(new Rectanglef(-0.5f, -0.5f, 0.5f, 0.5f), new Vector3f(0.6f, 0.3f, 0.0f), shaderPath) };
-		SLAE.execute("Add GUIObject", data);
+		Object[] data = { Render.MAIN_MENU, new GUIObject(new Rectanglef(1f, 1f, -1f, -1f), Color.white, shaderPath) };
+		SLAE.execute("Add GUIObject", data.clone());
+		data[1] = new GUIObject(new Rectanglef(-1f, 1f, 1f, .75f), Color.gray, shaderPath);
+		SLAE.execute("Add GUIObject", data.clone());
+		Object[] data2 = { new HUDObject(new Vector2i(0, 0), new Vector2i(100, 100), () -> {
+
+		}) };
+		SLAE.execute("Add HUDObject", data2);
 	}
 
 	@Override
